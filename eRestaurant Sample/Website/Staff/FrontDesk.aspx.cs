a09878c8092b9a044acd026ece647b0f106e3449
Max Mayo <mmayo2@studentmail.nait.ca>
2014-10-23 09:38:06 -0600

    Created AdHocController, made new form, clockpicker

--- eRestaurant Sample/Website/Staff/FrontDesk.aspx.cs
+++ eRestaurant Sample/Website/Staff/FrontDesk.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using eRestaurant.BLL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,5 +11,18 @@ public partial class Staff_FrontDesk : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+    protected void MockLastBillingDateTime_Click(object sender, EventArgs e)
+    {
+        MessageUserControl.TryRun(SetMockedTimeToLastBill);
+    }
+    protected void SetMockedTimeToLastBill()
+    {
+        var controller = new AdHocController();
+        var info = controller.GetLastBillDateTime();
+
+        SearchDate.Text = info.ToString("yyyy-MM-dd");
+
+        SearchTime.Text = info.ToString("HH:mm:ss");
     }
 }
